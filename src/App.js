ac95b1a9fb6bee5d506f3d7aeb482041d61268e7
CarryMain <mr.turanov98@gmail.com>
2024-01-09 14:16:37 +0300

    React Fragment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
+
+class WhoAmI extends Component { 
+  constructor(props) { 
+    super(props)
+    this.state = { 
+      years: 27,
+      position: ''
+    }
+    // this.nextYear = this.nextYear.bind(this)
+
+  }
+
+  nextYear(){
+    this.setState({
+      years: this.state.years + 1
+    })
+  }
+
+  commitInputChanges = (e, color) => {
+    console.log(color)
+    this.setState({
+      position: e.target.value
+    })
+  }
+  
+  render() { 
+    const {name, surname, link} = this.props
+    const {position, years} = this.state 
+
+    return ( 
+      <>
+        <button onClick={ () => this.nextYear()}>+++</button>
+        <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
+        <a href = {link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+        </form>
+      </>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name='John' surname="Smith" link="google.com"/>
+      <WhoAmI name='Alex' surname="Turanov" link="mail.ru"/>
     </div>
   );
 }
